93ad63aae220b12abdef1a59903de21dbb7e575e
Santhoshi Boyina <boyinasanthoshi12@gmail.com>
2025-06-24 17:07:28 +0530

    binary search sum of pairs

---
+++ two-pointer/pair-with-given-sum/binary-search.cpp
@@ -0,0 +1,66 @@
+class Solution {
+  public:
+  
+  void merge(vector<int> &arr, int low, int mid, int high) {
+    int p1 = low, p2 = mid+1, k=0;
+    vector<int> temp (high - low + 1);
+    while(p1 <= mid && p2 <= high) {
+        if(arr[p1] < arr[p2]) {
+            temp[k++] = arr[p1++];
+        }
+        else {
+            temp[k++] = arr[p2++];
+        }
+    }
+
+    while(p1 <= mid) {
+        temp[k++] = arr[p1++];
+    }
+    while(p2 <= high) {
+        temp[k++] = arr[p2++];
+    }
+
+    for(int i=0; i<(high-low+1); i++) {
+        arr[i+low] = temp[i];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high) {
+    if(low >= high)
+        return;
+    int mid = (low + high)/2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid+1, high);
+    merge(arr, low, mid, high);
+}
+
+bool binarySearch(vector<int> &arr, int low, int high, int target) {
+    if(low > high)
+        return false;
+    while(low <= high) {
+        int mid = (low + high)/2;
+        if(arr[mid] == target) {
+            return true;
+        }
+        else if(arr[mid] < target) {
+            return binarySearch(arr, mid+1, high, target);
+        }
+        else {
+            return binarySearch(arr, low, mid-1, target);
+        }
+    }
+}
+  
+    bool twoSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        mergeSort(arr, 0, n-1);
+        
+        for(int i=0; i<n; i++) {
+            int x = target - arr[i];
+            if(binarySearch(arr, i+1, n-1, x))
+                return true;
+        }
+        return false;
+        
+    }
+};
