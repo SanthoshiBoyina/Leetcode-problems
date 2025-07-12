e4268be340abd8b2bbab4b6690c390e5b47a9988
Santhoshi Boyina <boyinasanthoshi12@gmail.com>
2025-07-12 15:11:11 +0530

    anagram solution

---
+++ anagram/solution1.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length() != t.length())
+            return false;
+        int n = s.length();
+        vector<int> count1 (26, 0), count2 (26, 0);
+        for(int i=0; i<n; i++) {
+            count1[s[i] - 'a']++;
+            count2[t[i] - 'a']++;
+        }
+
+        for(int i=0; i<26; i++) {
+            if(count1[i] != count2[i])
+                return false;
+        }
+        return true;
+    }
+};
